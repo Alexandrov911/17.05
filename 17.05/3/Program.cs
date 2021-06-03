74ef212825d92733f0b26a4faf22ca0449693299
Alexandrov911 <sashok17062002@mail.ru>
2021-06-03 11:01:28 +0300

    17.05

---
+++ 17.05/3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.89
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double k = 0;
+            if (k < x)
+            {
+                k = Math.Pow(x, 2);
+            }
+            else
+            {
+                k = Math.Abs(x);
+            }
+            double y = 0;
+            if (Math.Sin(x) >= 0)
+            {
+                y=k*x;
+            }
+            else
+            {
+                y = k+x;
+            }
+            
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 17.05/4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = 0;
+            if (x<=0)
+            {
+                y = 0;
+            }
+            else if (x>0 && x <=1)
+            {
+                y = x;
+            }
+            else
+            {
+                y = x * x;
+            }
+
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
